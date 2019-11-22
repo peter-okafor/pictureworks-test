205158caf5bc1688a22895a7562d76b0b27a6cb5
Peter Okafor <peter.obinna.okafor@gmail.com>
2019-11-22 19:33:10 +0100

    feat: functionality for appending comment to the user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,4 +19,32 @@ class UserController extends Controller
         }
         
     }
+
+    public function add(Request $request){
+        //we are using validation to ensure the field exists
+        $this->validate($request, [
+            "id" => "required",
+            "password" => "required",
+            "comments" => "required"
+        ]);
+
+        if ($request->password != '720DF6C2482218518FA20FDC52D4DED7ECC043AB') return response()->json('invalid password', 401);
+
+        if (!is_numeric($request->id)) return response()->json('invalid id', 422);
+
+        $user = User::find($request->id);
+
+        if (!isset($user)) {
+            return response()->json('No such user (1)');
+        }
+        
+        $user->comments .= "\n".$request->comments; 
+
+        if ($user->save()) {
+            return response()->json('OK');
+        } else {
+            return response()->json('Could not update database', 500);
+        }
+        
+    }
 }
